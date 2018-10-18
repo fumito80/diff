e166ed8a2a26d4f0bcd8a05997328facf14dee71
Fumito Fujiwara <fumito80@gmail.com>
2018-10-19 01:12:10 +0900

    refactoring diff.ts

--- src/diff.ts
+++ src/diff.ts
@@ -1,126 +1,112 @@
 'use strict';
 
+enum DiffType {
+  DELETE = -1,
+  COMMON = 0,
+  ADD    = 1
+}
+
+type PathPosi = { x: number, y: number, k: number };
+type Ses = { elem: string, t: DiffType }[];
+
+function init(a: string, b: string): [string, string, number, number, boolean] {
+  const m = a.length;
+  const n = b.length;
+  if (m >= n) {
+    return [b, a, n, m, true];
+  }
+  return [a, b, m, n, false];
+};
+
+const P = (x: number, y: number, k: number): PathPosi => ({ x, y, k });
+
+function snake(k, p, pp, a, b, m, n, path, offset, pathpos) {
+  
+  function incXY(x: number, y: number): [number, number] {
+    if (!(x < m && y < n && a[x] === b[y])) {
+      return [x, y];
+    }
+    return incXY(x + 1, y + 1);
+  }
+
+  const dir = p > pp ? DiffType.DELETE : DiffType.ADD;
+  const r = path[k + dir + offset];
+  const y1 = Math.max(p, pp);
+  const [x, y] = incXY(y1 - k, y1);
+
+  path[k + offset] = pathpos.length;
+  pathpos[pathpos.length] = P(x, y, r);
+  return y;
+};
+
+const pipe = (fn: any, ...fns: any[]) => (arg: any) => fns.reduce((acc, fn2) => fn2(acc), fn(arg));
+
+function recordseq(epc: PathPosi[], a: string, b: string, reverse: boolean) {
+  function selctPath([ses, px, py, idx]) {
+    if (epc[idx].y - epc[idx].x > py - px) {
+      return [ses, b[py], reverse ? DiffType.DELETE : DiffType.ADD, px, py + 1];
+    } else if (epc[idx].y - epc[idx].x < py - px) {
+      return [ses, a[px], reverse ? DiffType.ADD : DiffType.DELETE, px + 1, py];
+    }
+    return [ses, a[px], DiffType.COMMON, px + 1, py + 1];
+  }
+
+  const buildSes = ([ses, elem, t, px, py]) => [([] as Ses).concat(ses, { elem, t }), px, py];
+
+  function calcSes(idx: number) {
+    return ([ses, px, py]) => {
+      if (!(px < epc[idx].x || py < epc[idx].y)) {
+        return [ses, px, py];
+      }
+      return pipe(selctPath, buildSes, calcSes(idx))([ses, px, py, idx]);
+    }
+  }
+
+  function decEpc(idx: number) {
+    return ([ses, px, py]: [Ses, number, number]) => {
+      if (idx < 0) {
+        return ses as Ses;
+      }
+      return pipe(calcSes(idx), decEpc(idx - 1))([ses, px, py]) as Ses;
+    }
+  }
+
+  return decEpc(epc.length - 1)([[] as Ses, 0, 0]);
+};
+
 export namespace Diff {
-
-  export const SES_DELETE = -1;
-  export const SES_COMMON = 0;
-  export const SES_ADD    = 1;
-
-  type Pathpos = { x: number, y: number, k: number };
-
-  function init(_a, _b) {
-    const m = _a.length;
-    const n = _b.length;
-    if (m >= n) {
-      return [_b, _a, n, m, true];
-    }
-    return [_a, _b, m, n];
-  };
-
-  const P = (x, y, k): Pathpos => ({ x, y, k });
-
-  function snake(k, p, pp, a, b, m, n, path, offset, pathpos) {
-    
-    function incXY(x, y) {
-      if (!(x < m && y < n && a[x] === b[y])) {
-        return [x, y];
-      }
-      return incXY(x + 1, y + 1);
-    }
-
-    const dir = p > pp ? -1 : 1;
-    const r = path[k + dir + offset];
-    const y1 = Math.max(p, pp);
-    const [x, y] = incXY(y1 - k, y1);
-
-    path[k + offset] = pathpos.length;
-    pathpos[pathpos.length] = P(x, y, r);
-    return y;
-  };
-
-  function recordseq(epc, a, b, reverse) {
-    const seselem = (elem, t) => ({ elem, t });
-    let px_idx = 0;
-    let py_idx = 0;
-
-    let ses: any[] = [];
-    // let lcs = 0;
-
-    for (let i = epc.length-1; i >= 0; --i) {
-      while(px_idx < epc[i].x || py_idx < epc[i].y) {
-        if (epc[i].y - epc[i].x > py_idx - px_idx) {
-          ses[ses.length] = seselem(b[py_idx], reverse ? Diff.SES_DELETE : Diff.SES_ADD);
-          ++py_idx;
-        } else if (epc[i].y - epc[i].x < py_idx - px_idx) {
-          ses[ses.length] = seselem(a[px_idx], reverse ? Diff.SES_ADD : Diff.SES_DELETE);
-          ++px_idx;
-        } else {
-          ses[ses.length] = seselem(a[px_idx], Diff.SES_COMMON);
-          // lcs += a[px_idx];
-          ++px_idx;
-          ++py_idx;
-        }
-      }
-    }
-    return ses;
-  };
-
-  export function diff(a, b) {
-    const [_a, _b, m, n, reverse] = init(a, b);
+  export const { DELETE, COMMON, ADD } = DiffType;
+  export function diff(str1: string, str2: string) {
+    const [a, b, m, n, reverse] = init(str1, str2);
     const offset = m + 1;
-
-    // let ed = 0;
-    // let ses = [];
-    // let lcs = '';
-
-    // return {
-      // SES_DELETE : -1,
-      // SES_COMMON :  0,
-      // SES_ADD    :  1,
-
-      // editdistance: function () {
-      //   return ed;
-      // },
-      // getlcs: function () {
-      //   return lcs;
-      // },
-      // getses: function () {
-      //   return ses;
-      // },
-      // compose: function () {
-        let path = {};
-        let pathpos: Pathpos[] = [];
-        const delta = n - m;
-        const size  = m + n + 3;
-        let fp = {};
-        for (let i = 0; i < size; ++i) {
-          fp[i] = -1;
-          path[i] = -1;
-        }
-        let p = -1;
-        do {
-          ++p;
-          for (let k = -p; k <= delta - 1; ++k) {
-            fp[k+offset] = snake(k, fp[k-1+offset]+1, fp[k+1+offset], a, b, m, n, path, offset, pathpos);
-          }
-          for (let k= delta + p; k >= delta + 1; --k) {
-            fp[k+offset] = snake(k, fp[k-1+offset]+1, fp[k+1+offset], a, b, m, n, path, offset, pathpos);
-          }
-          fp[delta+offset] = snake(delta, fp[delta-1+offset]+1, fp[delta+1+offset], a, b, m, n, path, offset, pathpos);
-        } while (fp[delta+offset] !== n);
-        
-        // ed = delta + 2 * p;
-        
-        let r = path[delta + offset];
-        
-        const epc: Pathpos[] = [];
-        while (r !== -1) {
-          epc.push(P(pathpos[r].x, pathpos[r].y, null));
-          r = pathpos[r].k;
-        }
-        const ses = recordseq(epc, a, b, reverse);
-        return ses;
-      // }
-    // };
+    let path = {};
+    let pathpos: PathPosi[] = [];
+    const delta = n - m;
+    const size  = m + n + 3;
+    let fp = {};
+    for (let i = 0; i < size; ++i) {
+      fp[i] = -1;
+      path[i] = -1;
+    }
+    let p = -1;
+    do {
+      ++p;
+      for (let k = -p; k <= delta - 1; ++k) {
+        fp[k+offset] = snake(k, fp[k-1+offset]+1, fp[k+1+offset], a, b, m, n, path, offset, pathpos);
+      }
+      for (let k= delta + p; k >= delta + 1; --k) {
+        fp[k+offset] = snake(k, fp[k-1+offset]+1, fp[k+1+offset], a, b, m, n, path, offset, pathpos);
+      }
+      fp[delta+offset] = snake(delta, fp[delta-1+offset]+1, fp[delta+1+offset], a, b, m, n, path, offset, pathpos);
+    } while (fp[delta+offset] !== n);
+    
+    let r = path[delta + offset];
+    
+    const epc: PathPosi[] = [];
+    while (r !== -1) {
+      epc.push(P(pathpos[r].x, pathpos[r].y, 0));
+      r = pathpos[r].k;
+    }
+    return recordseq(epc, a, b, reverse);
   }
 }
--- src/difftest.ts
+++ src/difftest.ts
@@ -12,14 +12,12 @@ var ses = Diff.diff(process.argv[2], process.argv[3]);
 //   process.stderr.write(part.elem[color]);
 // });
 
-console.log("ses");
-
 for (let i = 0; i < ses.length; ++i) {
-  if (ses[i].t === Diff.SES_COMMON) {
+  if (ses[i].t === Diff.COMMON) {
     console.log(" " + ses[i].elem);
-  } else if (ses[i].t === Diff.SES_DELETE) {
+  } else if (ses[i].t === Diff.DELETE) {
     console.log("-" + ses[i].elem);
-  } else if (ses[i].t === Diff.SES_ADD) {
+  } else if (ses[i].t === Diff.ADD) {
     console.log("+" + ses[i].elem);
   }
 }
