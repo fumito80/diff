51f8d309762e70799aeafed78f31d64f454e4bfd
Fumito Fujiwara <fumito80@gmail.com>
2018-10-26 01:08:09 +0900

    something

--- src/diff.ts
+++ src/diff.ts
@@ -19,8 +19,6 @@ enum DiffType {
 }
 
 type Ses = { elem: string, t: DiffType }[];
-type Epc = [number, number]; // x, y
-// type PathPos = [number, number, number]; // x, y, k
 type PathList = {
   x: number,
   y: number,
@@ -51,44 +49,7 @@ function init(_a: string | string[], _b: string | string[]): Source {
   return ret(String(_a), String(_b));
 };
 
-function recordseq(epc: Epc[], a: string | string[], b: string | string[], reverse: boolean): Ses {
-  function selectPath(diffYX: number) {
-    return ([px, py]: [number, number]) => {
-      if (diffYX === py - px) {
-        return [a[px], DiffType.COMMON, px + 1, py + 1];
-      }
-      if (diffYX > py - px) {
-        return [b[py], reverse ? DiffType.DELETE : DiffType.ADD, px, py + 1];
-      }
-      // if (y - x < py - px)
-      return [a[px], reverse ? DiffType.ADD : DiffType.DELETE, px + 1, py];
-    }
-  }
-  const ses: Ses = [];
-  epc.reduce(([px, py], [x, y]) => {
-    return recurse(
-      ([px, py]) => (px < x || py < y),
-      pipe(
-        selectPath(y - x),
-        ([elem, t, px, py]) => [px, py, ses.push({ elem, t })]
-      )
-    )([px, py]);
-  }, [0, 0]);
-  return ses;
-};
-
-function snake({ a, b, m, n }: Source) {
-  return ([k, p, pp]): [number, number, number, number] => {
-    const [y1, dir] = p > pp ? [p, DiffType.DELETE] : [pp, DiffType.ADD];
-    const [x, y] = recurse<[number, number]>(
-        ([x, y]) => (x < m && y < n && a[x] === b[y]),
-        ([x, y]) => [x + 1, y + 1]
-      )([y1 - k, y1]);
-    return [k, dir, x, y];
-  }
-};
-
-function diff2({ a, b }: Source, pathList: PathList): Ses {
+function diff2({ a, b, flip }: Source, pathList: PathList): Ses {
   const ses: Ses = [];
   recurse<PathList>(
     ({ parent }) => parent != null,
@@ -101,36 +62,45 @@ function diff2({ a, b }: Source, pathList: PathList): Ses {
         ses.unshift({ "elem": a.slice(x - sameLen, x) as string, "t": DiffType.COMMON });
       }
       if (diffY < diffX) {
-          ses.unshift({ "elem": a[parent.x] as string, "t": DiffType.DELETE });
+        ses.unshift({ "elem": a[parent.x] as string, "t": flip ? DiffType.ADD : DiffType.DELETE });
       } else if (diffX < diffY) {
-        ses.unshift({ "elem": b[parent.y] as string, "t": DiffType.ADD });
+        ses.unshift({ "elem": b[parent.y] as string, "t": flip ? DiffType.DELETE : DiffType.ADD });
       }
-
       return parent;
     }
   )(pathList);
   return ses;
 }
 
+function snake({ a, b, m, n }: Source) {
+  return ([k, p, pp]): [number, number, number, number] => {
+    const [y1, dir] = p > pp ? [p, DiffType.DELETE] : [pp, DiffType.ADD];
+    const [x, y] = recurse<[number, number]>(
+        ([x, y]) => (x < m && y < n && a[x] === b[y]),
+        ([x, y]) => [x + 1, y + 1]
+      )([y1 - k, y1]);
+    return [k, dir, x, y];
+  }
+};
+
 export namespace Diff {
   export const { DELETE, COMMON, ADD } = DiffType;
   export function diff(str1: string | string[], str2: string | string[]) {
     const source = init(str1, str2);
-    const offset = source.m + 1;
-    const delta  = source.n - source.m;
-    const size   = source.m + source.n + 3;
+    const { m, n } = source;
+    const offset = m + 1;
+    const delta = n - m;
+    const size = m + n + 3;
     const pathList: PathList[] = [];
-    const epc: Epc[] = [];
     const path = new Array<{ "k": number, "fp": number }>(size).fill({ "k": -1, "fp": -1 });
 
     function setPath([k, dir, x, y]) {
       path[k + offset] = { "k": pathList.length, "fp": y };
       pathList.push({ x, y, parent: pathList[path[k + dir + offset].k] });
-      // pathpos.push([x, y, path[k + dir + offset].k]);
     }
 
     recurse<number>(
-      _ => (path[delta + offset].fp !== source.n),
+      _ => (path[delta + offset].fp !== n),
       p => {
         ([
           [- p      , ([k]) => k < delta, ([k]) => [k, path[k - 1 + offset].fp + 1, path[k + 1 + offset].fp],   1],
@@ -145,18 +115,7 @@ export namespace Diff {
       }
     )(0);
 
-    // console.log(JSON.stringify(pathList[path[delta + offset].k], null, 4));
+    // console.log(JSON.stringify(pathList, null, 4));
     return diff2(source, pathList[path[delta + offset].k]);
-    // recurse(
-    //   r => r !== -1,
-    //   r => {
-    //     const [x, y, k] = pathpos[r];
-    //     epc.unshift([x, y]);
-    //     return k;
-    //   }
-    // )(path[delta + offset].k);
-
-    // console.log(JSON.stringify(epc, null, 4));
-    // return recordseq(epc, a, b, reverse);
   }
 }
--- test/difftest.js
+++ test/difftest.js
@@ -6,7 +6,7 @@ const path = require('path');
 const diff = require('wu-diff-js');
 const ONP = require('./onp');
 const jsDiff = require('diff');
-const jDiff = require('./jdiff');
+// const jDiff = require('./jdiff');
 
 console.log(diff.default);
 
@@ -27,76 +27,84 @@ let src2 = [
 src1 = 'ABCDA';
 src2 = 'BFEABD';
 
-
-src1 = fs.readFileSync(path.join(__dirname, 'txt-codemirror-man1.txt')).toLocaleString();
-src2 = fs.readFileSync(path.join(__dirname, 'txt-codemirror-man2.txt')).toLocaleString();
+// src1 = fs.readFileSync(path.join(__dirname, 'txt-codemirror-man1.txt')).toLocaleString();
+// src2 = fs.readFileSync(path.join(__dirname, 'txt-codemirror-man2.txt')).toLocaleString();
 
 const start = Date.now();
 console.log('start: ' + start);
+mydiff();
+// jsdiff();
+// onp();
+// wudiff();
+let end = Date.now();
+console.log('end: ' + end + ', lap: ' + (end - start));
 
-const ses = Diff.diff(src1 || process.argv[2], src2 || process.argv[3]);
-console.log(ses);
+function mydiff() {
+  let ses = Diff.diff(src1 || process.argv[2], src2 || process.argv[3]);
+  console.log(ses);
 
-for (let i = 0; i < ses.length; ++i) {
-  if (ses[i].t === Diff.COMMON) {
-    console.log(" " + ses[i].elem);
-  } else
-  if (ses[i].t === Diff.DELETE) {
-    console.log("-" + ses[i].elem);
-  } else if (ses[i].t === Diff.ADD) {
-    console.log("+" + ses[i].elem);
+  for (let i = 0; i < ses.length; ++i) {
+    // if (ses[i].t === Diff.COMMON) {
+    //   console.log(" " + ses[i].elem);
+    // }
+    // else
+    if (ses[i].t === Diff.DELETE) {
+      console.log("-" + ses[i].elem);
+    } else if (ses[i].t === Diff.ADD) {
+      console.log("+" + ses[i].elem);
+    }
   }
 }
 
 // const jdiff = new jDiff.UnifiedDiff(src1 || process.argv[2], src2 || process.argv[3], 1000);
 // console.log(jdiff.toString());
 
-// const onp = new ONP.Diff(src1 || process.argv[2], src2 || process.argv[3]);
-// onp.compose();
-// const ses = onp.getses();
-// // console.log(ses);
+function onp() {
+  const onp = new ONP.Diff(src1 || process.argv[2], src2 || process.argv[3]);
+  onp.compose();
+  const ses = onp.getses();
+  // console.log(ses);
 
-// for (let i = 0; i < ses.length; ++i) {
-//   // if (ses[i].t === Diff.COMMON) {
-//   //   console.log(" " + ses[i].elem);
-//   // } else
-//   if (ses[i].t === onp.SES_DELETE) {
-//     console.log("-" + ses[i].elem);
-//   } else if (ses[i].t === onp.SES_ADD) {
-//     console.log("+" + ses[i].elem);
-//   }
-// }
+  for (let i = 0; i < ses.length; ++i) {
+    // if (ses[i].t === Diff.COMMON) {
+    //   console.log(" " + ses[i].elem);
+    // } else
+    if (ses[i].t === onp.SES_DELETE) {
+      console.log("-" + ses[i].elem);
+    } else if (ses[i].t === onp.SES_ADD) {
+      console.log("+" + ses[i].elem);
+    }
+  }
+}
 
-// const ses = diff.default(Array.from(src1 || process.argv[2]), Array.from(src2 || process.argv[3]));
+function wudiff() {
+  let ses = diff.default(src1 || process.argv[2], src2 || process.argv[3]);
 
-// for (let i = 0; i < ses.length; ++i) {
-//   // if (ses[i].t === Diff.COMMON) {
-//   //   console.log(" " + ses[i].elem);
-//   // } else
-//   if (ses[i].type === 'removed') {
-//     console.log("-" + ses[i].value);
-//   } else if (ses[i].type === 'added') {
-//     console.log("+" + ses[i].value);
-//   }
-// }
+  for (let i = 0; i < ses.length; ++i) {
+    // if (ses[i].t === Diff.COMMON) {
+    //   console.log(" " + ses[i].elem);
+    // } else
+    if (ses[i].type === 'removed') {
+      console.log("-" + ses[i].value);
+    } else if (ses[i].type === 'added') {
+      console.log("+" + ses[i].value);
+    }
+  }
+}
 
-// const one = 'beep boop';
-// const other = 'beep boob blah';
- 
-// var jsdiff = jsDiff.diffChars(src1 || process.argv[2], src2 || process.argv[3]);
- 
-// jsdiff.forEach(function(part){
-//   // green for additions, red for deletions
-//   // grey for common parts
-//   // var color = part.added ? 'green' :
-//   //   part.removed ? 'red' : 'grey';
-//   // process.stderr.write(part.value[color]);
-//   if (part.removed) {
-//     console.log("-" + part.value);
-//   } else if (part.added) {
-//     console.log("+" + part.value);
-//   }
-// });
-
-let end = Date.now();
-console.log('end: ' + end + ', lap: ' + (end - start));
+function jsdiff() {
+  let jsdiff = jsDiff.diffChars(src1 || process.argv[2], src2 || process.argv[3]);
+  
+  jsdiff.forEach(function(part){
+    // green for additions, red for deletions
+    // grey for common parts
+    // var color = part.added ? 'green' :
+    //   part.removed ? 'red' : 'grey';
+    // process.stderr.write(part.value[color]);
+    if (part.removed) {
+      console.log("-" + part.value);
+    } else if (part.added) {
+      console.log("+" + part.value);
+    }
+  });
+}
