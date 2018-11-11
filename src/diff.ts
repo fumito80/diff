1643919e9a0f27c74324352ea9d8f283e5e2aebe
Fujiwara Fumito <fumito80@gmail.com>
2018-11-12 01:27:43 +0900

    modify diff.ts

--- src/diff.ts
+++ src/diff.ts
@@ -36,8 +36,8 @@ type Source = {
   "b": string | string[],
   "m": number,
   "n": number,
-  "na"?: number,
-  "nb"?: number,
+  "nL": number,
+  "nR": number,
   "flip": boolean
 };
 
@@ -70,15 +70,15 @@ enum ElemType { added, removed, common };
 function init({ a = '', b = '' }: { a: string | string[], b: string | string[] }): Source {
   const [m, n] = [a.length, b.length];
   function split({ a, b, m, n, flip }: Source): Source {
-    const na = Math.ceil(n / 2);
-    const nb = Math.trunc(n / 2);
-    return { a, b, m, n, na, nb, flip };
+    const nL = Math.ceil(n / 2);
+    const nR = Math.trunc(n / 2);
+    return { a, b, m, n, nL, nR, flip };
   }
   function orFlip({ a, b }): Source {
     if (m >= n) {
-      return split({ "a": b, "b": a, "m": n, "n": m, "flip": true });
+      return split({ "a": b, "b": a, "m": n, "n": m, "flip": true, "nL": 0, "nR": 0 });
     }
-    return split({ a, b, m, n, "flip": false });
+    return split({ a, b, m, n, "flip": false, "nR": 0, "nL": 0  });
   }
   if ((typeof a === 'string' && typeof b === 'string') || (Array.isArray(a) && Array.isArray(b))) {
     return orFlip({ a, b });
@@ -147,101 +147,82 @@ function unifiedResultR({ a, b, m, n, flip }: Source, head: Path) {
     }
     return [];
   }
-  // function getAcc([undiff, diffOrNull, [diff], acc]: Ses[][]): Ses[] {
-  //   const [prev] = acc.slice(- 1);
-  //   const tail = acc.slice(0, - 1);
-  //   if (prev && ((prev.added && diff.added) || (prev.removed && diff.removed))) {
-  //     return [...tail, Object.assign({}, prev, { "value": prev.value + diff.value })];
-  //   }
-  //   return [...acc, ...undiff, ...diffOrNull];
-  // }
-  function getAcc([undiff, diffOrNull, [diff], [prev, ...tail], acc]: Ses[][]): Ses[] {
+  function getAcc([undiff, diffOrNull, [diff], acc]: Ses[][]): Ses[] {
+    const [prev] = acc.slice(- 1);
+    const tail = acc.slice(0, - 1);
     if (prev && ((prev.added && diff.added) || (prev.removed && diff.removed))) {
-      return [Object.assign({}, prev, { "value": diff.value + prev.value }), ...tail];
+      return [...tail, Object.assign({}, prev, { "value": prev.value + diff.value })];
     }
-    return [...undiff, ...diffOrNull, ...acc];
+    return [...acc, ...undiff, ...diffOrNull];
   }
-  // const [, result] = recurse<[Path, Ses[]]>(
-  //   ([{ x }]) => x > 0,
-  //   ([{ x, y, parent = { x: 0, y: 0 } }, acc]) => {
-  //     const diffX = x - parent.x;
-  //     const diffY = y - parent.y;
-  //     const undiff = getUndiff(x, Math.min(diffX, diffY));
-  //     const diffOrNull = getDiff(diffX - diffY, parent);
-  //     return [parent, getAcc([undiff, diffOrNull, [...undiff, ...diffOrNull], acc, acc])];
-  //   }
-  // )([head, []]);
-  // return result;
-  function unified(pathList, reduceFun, nextFun) {
-    return reduceFun.call(pathList, (acc: Ses[], { x, y, parent = { x: 0, y: 0 } }) => {
+  const [, result] = recurse<[Path, Ses[]]>(
+    ([{ x }]) => x > 0,
+    ([{ x, y, parent = { x: 0, y: 0 } }, acc]) => {
       const diffX = x - parent.x;
       const diffY = y - parent.y;
       const undiff = getUndiff(x, Math.min(diffX, diffY));
       const diffOrNull = getDiff(diffX - diffY, parent);
-      return getAcc([undiff, diffOrNull, [...undiff, ...diffOrNull], acc, acc]);
-      // const [last] = acc.slice(-1);
-      // const [next] = nextFun(diff, undiff);
-      // if (last && next && ((last.added && next.added) || (last.removed && next.removed))) {
-      //   const { added, removed } = last;
-      //   return [...acc.slice(0, -1), { "value": last.value + next.value, added, removed }, ...undiff] as Ses[];
-      // }
-      // return [...acc, ...nextFun(diff, undiff)] as Ses[];
-    }, [] as Ses[]);
- }
- const pathList = linkedListToArray(head, 'parent');
- return unified(pathList, Array.prototype.reduceRight, (diff, undiff) => [...diff, ...undiff]);
+      return [parent, getAcc([undiff, diffOrNull, [...undiff, ...diffOrNull], acc])];
+    }
+  )([head, []]);
+  return result;
+//   function getAcc([undiff, diffOrNull, [diff], [prev, ...tail], acc]: Ses[][]): Ses[] {
+//     if (prev && ((prev.added && diff.added) || (prev.removed && diff.removed))) {
+//       return [Object.assign({}, prev, { "value": diff.value + prev.value }), ...tail];
+//     }
+//     return [...undiff, ...diffOrNull, ...acc];
+//   }
+//   function unified(pathList, reduceFun, nextFun) {
+//     return reduceFun.call(pathList, (acc: Ses[], { x, y, parent = { x: 0, y: 0 } }) => {
+//       const diffX = x - parent.x;
+//       const diffY = y - parent.y;
+//       const undiff = getUndiff(x, Math.min(diffX, diffY));
+//       const diffOrNull = getDiff(diffX - diffY, parent);
+//       return getAcc([undiff, diffOrNull, [...undiff, ...diffOrNull], acc, acc]);
+//     }, [] as Ses[]);
+//  }
+//  const pathList = linkedListToArray(head, 'parent');
+//  return unified(pathList, Array.prototype.reduceRight, (diff, undiff) => [...diff, ...undiff]);
 }
 
 /**
  * Snake
  */
-function Snake(condition: {([x, y]: [number, number]): boolean }) {
-  return ([k, dir, x, y]: number[]): number[] => {
-    const [xx, yy] = recurse<[number, number]>(
-        condition,
-        ([xx, yy]) => [xx + 1, yy + 1]
-      )([x, y]);
-    return [k, dir, xx, yy];
-  }
-};
+// function Snake(condition: {([x, y]: [number, number]): boolean }) {
+//   return ([k, dir, x, y]: number[]): number[] => {
+//     const [xx, yy] = recurse<[number, number]>(
+//         condition,
+//         ([xx, yy]) => [xx + 1, yy + 1]
+//       )([x, y]);
+//     return [k, dir, xx, yy];
+//   }
+// };
 
 const snakeConditionL: SnakeCondition = ({ a, b, m, n }) => ([x, y]) => x < m && y < n && a[x] === b[y];
 const snakeConditionR: SnakeCondition = ({ a, b, m, n }) => ([x, y]) => x < m && y < n && a[m - x - 1] === b[n - y - 1];
 
-/**
- * Diff main
- */
-
-export function diff(a: string | string[], b: string | string[]) {
-  const source = init({ a, b });
-  const { m, n, nb } = source;
-  const offset = m + 1;
-  const delta = n - m;
-  const fpMax = m + n + 3;
-  const paths: Path[] = [];
-  const fp: Fp[] = new Array(fpMax).fill({ "fp": - 1, "k": - 1 });
-
-  const rangeKN = [...Array(n)].map((_, i) => - (n - i) + delta);
-  const rangeKM = [...Array(m + 1)].map((_, i) => m - i + delta);
-
-  const snakeL = pipe<void>(preSnake, Snake(snakeConditionL(source)), postSnake);
-  const snakeR = pipe<void>(preSnake, Snake(snakeConditionR(source)), postSnake);
-
-  function preSnake(k: number): number[] {
+function Snake(offset: number, fp: Fp[], paths: Path[], condition: {([x, y]: [number, number]): boolean }) {
+  return (k: number) => {
     const [p, pp] = [fp[k - 1 + offset].fp + 1, fp[k + 1 + offset].fp];
     const [y1, dir] = p > pp ? [p, -1] : [pp, 1];
-    return [k, dir, y1 - k, y1];
-  }
-
-  function postSnake([k, dir, x, y]): void {
+    const [x, y] = recurse<[number, number]>(
+      condition,
+      ([x, y]) => [x + 1, y + 1]
+    )([y1 - k, y1]);
     fp[k + offset] = { "fp": y, "k": paths.length };
     const parent = paths[fp[k + dir + offset].k];
     paths.push({ x, y, parent });
   }
+};
 
-  function onp(n: number, snake: { (x): void }): Path {
+function Onp(source: Source, offset: number, delta: number, rangeKN: number[], rangeKM: number[]) {
+  return (nMax: number, snakeCondition: SnakeCondition): Path => {
+    const { m, n } = source;
+    const paths: Path[] = [];
+    const fp: Fp[] = new Array(m + nMax + 3).fill({ "fp": - 1, "k": - 1 });
+    const snake = Snake(offset, fp, paths, snakeCondition(source));
     const [, { k }] = recurse<[number, Fp]>(
-      ([, { fp }]) => fp < n,
+      ([, { fp }]) => fp < nMax,
       ([p]) => {
         rangeKN.slice(n - p).forEach(snake);
         rangeKM.slice(m - p + delta).forEach(snake);
@@ -250,10 +231,28 @@ export function diff(a: string | string[], b: string | string[]) {
     )([delta, { "fp": - 1, "k": - 1 }]);
     return paths[k];
   }
+}
 
-  const l = (...ss) => ss.forEach(s => console.log(JSON.stringify(s, null, 2)));
+const log = (...ss) => ss.forEach(s => console.log(JSON.stringify(s, null, 2)));
 
-  const head = onp(n, snakeR);
-  l(head);
-  return unifiedResultR(source, head);
+/**
+ * Diff main
+ */
+
+export function diff(a: string | string[], b: string | string[]) {
+  const source = init({ a, b });
+  const { m, n, nL, nR } = source;
+  const offset = m + 1;
+  const delta = n - m;
+
+  const rangeKN = [...Array(n)].map((_, i) => - (n - i) + delta);
+  const rangeKM = [...Array(m + 1)].map((_, i) => m - i + delta);
+
+  const onp = Onp(source, offset, delta, rangeKN, rangeKM);
+
+  const headL = onp(nL, snakeConditionL);
+  const headR = onp(nR, snakeConditionR);
+  log(headL, headR);
+  return [...unifiedResult(source, headL), ...unifiedResultR(source, headR)];
+  // return unifiedResultR(source, headR);
 }
